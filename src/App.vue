3e4106186c9bbdb64eabe4d922401d180c70b269
Vladislav Saleev <eventflex1@gmail.com>
2022-01-08 20:37:08 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <h1 class="game-title">Simon game</h1>
+    <Simon />
+  </div>
+</template>
+
+<script>
+import Simon from "@/components/Simon";
+export default {
+  name: "App",
+  components: {
+    Simon,
+  },
+};
+</script>
+
+<style>
+* {
+  background-color: rgb(54, 57, 63);
+}
+.game-title {
+  text-align: center;
+  color: whitesmoke;
+  font-size: 40px;
+}
+</style>
---
+++ src/components/Playfield.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="field-wrapper">
+    <div
+      @click="sendPlayerMove('green')"
+      class="block"
+      :class="{ opacity: opacityToggle.green }"
+      style="background: green; border-top-left-radius: 50px"
+    ></div>
+    <div
+      @click="sendPlayerMove('blue')"
+      class="block"
+      :class="{ opacity: opacityToggle.blue }"
+      style="background: blue; border-top-right-radius: 50px"
+    ></div>
+    <div
+      @click="sendPlayerMove('red')"
+      class="block"
+      :class="{ opacity: opacityToggle.red }"
+      style="background: red; border-bottom-left-radius: 50px"
+    ></div>
+    <div
+      @click="sendPlayerMove('yellow')"
+      class="block"
+      :class="{ opacity: opacityToggle.yellow }"
+      style="background: yellow; border-bottom-right-radius: 50px"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    opacityToggle: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    sendPlayerMove(color) {
+      this.$emit("sendPlayerMove", color);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.field-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-left: 150px;
+  width: 550px;
+  height: 540px;
+  margin-right: 50px;
+}
+.block {
+  height: 250px;
+  width: 250px;
+  padding: 10px;
+  opacity: 0.5;
+}
+.opacity {
+  opacity: 1;
+}
+</style>
